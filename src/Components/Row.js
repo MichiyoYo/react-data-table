93604b404660898db5772c2114fb1493be5b6f74
Cristina Lester <cristina.deelester@gmail.com>
2022-02-08 10:48:37 -0800

    added zebra coloring

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { css } from "@emotion/css";
+
+const rowStyle = css`
+  background: blue;
+  text-align: center;
+  padding: 10px;
+  background: #ffffffc2;
+  &:nth-child(even) {
+    background: #f5f5f5c2;
+  }
+  td {
+    padding: 10px;
+  }
+`;
 
 function Row({ user }) {
   const fields = Object.values(user);
   return (
-    <tr>
+    <tr className={rowStyle}>
       {fields.map((field, i) => {
         if (typeof field !== "object") {
           return <td key={i}>{field}</td>;
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
 import { AppContext } from "../App";
 import Row from "./Row";
+import { css } from "@emotion/css";
+
+const tableStyle = css`
+  background-color: #ffffffa8;
+  border-radius: 10px;
+  border: 1px solid #fff;
+  th {
+    padding: 10px;
+  }
+`;
 
 function Table(props) {
   const { users } = useContext(AppContext);
   const headerFields = Object.keys(users[0]);
   return (
-    <table>
+    <table className={tableStyle}>
       <thead>
         <tr>
           {headerFields.map((field, i) => (
